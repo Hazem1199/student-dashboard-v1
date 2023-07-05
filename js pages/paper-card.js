13e2072be0d93621f7335d26aeecd88b9cccd9c7
Hazem Essam <hazemessam.brooklyn@gmail.com>
2023-07-05 18:54:32 +0300

    SRM

--- js pages/paper-card.js
+++ js pages/paper-card.js
@@ -12,6 +12,8 @@ var searchButton = document.querySelector('.search-button');
 var pic = document.getElementById("profile-pic")
 var headName = document.querySelector('.headName')
 const seeMore5 = document.querySelector('.seeMore5')
+const footer5 = document.querySelector('.footer5');
+
 
 
 
@@ -25,112 +27,143 @@ async function getPaper() {
 }
 
 
+
 async function showPaper(id) {
-    const students = await getPaper();
-    const filteredStudents = students.filter(student => student.ID === id);
+    const students = await getPaper(id);
+    let filteredPapers = students.filter(student => student.ID == id);
 
     let donePaperCount = 0;
-    let paperCount = 0;
-
-    for (let i = 0; i < filteredStudents.length; i++) {
-        const student = filteredStudents[i];
-        for (const key in student) {
-            if (key !== "ID" && key !== "Personal id" && student[key]) {
-                paperCount++;
-                if (student[key]) {
-                    donePaperCount++;
-                }
-            }
-        }
+    let paperCount = 3; // initialize paperCount with the correct value
+    filteredPapers.forEach(student => {
+        if (filteredPapers) {
+            // increment donePaperCount for each paper completed by the student
+            if (student['Personal id'] == true) donePaperCount++;
+            if (student.Certificate == true) donePaperCount++;
+            if (student['HR Letter'] == true) donePaperCount++;
+            document.querySelector('.num-Papers').textContent = `${donePaperCount} / ${paperCount}`;
+            console.log(donePaperCount);
+            const remainingPapers = [];
+      if (student['Personal id'] == false) remainingPapers.push('Personal id');
+      if (student.Certificate  == false) remainingPapers.push('Certificate');
+      if (student['HR Letter']  == false) remainingPapers.push('HR Letter');
+      if (remainingPapers.length === 0) {
+        footer5.textContent = `No paper(s) remaining`;
+      } else {
+        const remainingPapersText = remainingPapers.join(' and ');
+        footer5.textContent = `${remainingPapersText} paper(s) remaining`;
+      }
     }
-
-    console.log("done papers: " + donePaperCount);
-    console.log("total papers: " + paperCount);
-
-    const numPapers = document.querySelector('.num-Papers');
-    const footer5 = document.querySelector('.footer5');
-
-    if (donePaperCount === paperCount) {
-        numPapers.textContent = `${donePaperCount} / ${paperCount}`;
-        footer5.textContent = "No More Of paper";
-    } else {
-        numPapers.textContent = `${donePaperCount} / ${paperCount}`;
-        footer5.textContent = `${paperCount - donePaperCount} paper(s) remaining`;
-    }
+  });
 }
 
-// // Update the footer based on the next module deadline
-// const filteredModules = students.filter(student => {
-//   const date = new Date(student[`g${i} date`]);
-//   return date >= new Date();
-// });
-// const nextModule = filteredModules.sort((a, b) => new Date(a[`g${i} date`]) - new Date(b[`g${i} date`]))[0];
-// if (nextModule) {
-//   const formattedDueDate = new Intl.DateTimeFormat('en-US', { month: 'short', day: 'numeric', year: 'numeric' }).format(new Date(nextModule[`g${i} date`]));
-//   footer4.textContent = `Next Module: ${formattedDueDate}`;
-// } else {
-//   footer4.textContent = 'No upcoming Module';
-// }
-// let moduleUrl = `Group.html?id=${id}`;
-// seeMore4.href = moduleUrl;
-// let module = await fetch(moduleUrl);
-// let moduleData = await module.json();
-// localStorage.setItem('moduleData', JSON.stringify(moduleData));
-// window.open(moduleUrl);
 
 
 
 
-// async function showPaper(value) {
-//     var students = await getPaper();
-//     const numPapers = document.querySelector('.num-Papers');
-//     const footer5 = document.querySelector('.footer5');
-//     let filteredPapers = students.filter(student => student.ID == value);
-//     let paperCount = filteredPapers.length;
-//     console.log("paper :" + paperCount);
-
-//     const student = {
-//     "Personal id": true,
-//     "Certificate": true,
-//     "HR Letter": true
-//     };
-
-//     const personalIdKey = "Personal id";
-//     const certificateKey = "Certificate";
-//     const hrLetterKey = "HR Letter";
-
-//     const donePaperCount = personalIdKey == certificateKey == hrLetterKey;
-// const notDonePaperCount = donePaperCount.length - donePaperCount;
-
-
-
-//     console.log(donePaperCount);
-
-//     if (donePaperCount) {
-//     numPapers.textContent = `${donePaperCount} / ${paperCount}`;
-//     } else {
-//     numPapers.textContent = `${notDonePaperCount} / ${paperCount}`;
-//     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-searchButton.addEventListener('click', () => {
-    const value = searchInput[0].value;
-    showPaper(value)
+searchButton.addEventListener('click', async () => {
+    const id = searchInput[0].value;
+    showPaper(id)
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// display the number of completed papers
+
+
+// document.querySelector('.num-Papers').textContent = `${donePaperCount} / ${paperCount}`;
+
+// // Show the number of remaining papers
+// if (donePaperCount === paperCount) {
+//     footer5.textContent = "No More Of paper";
+// } else {
+//     footer5.textContent = `${paperCount - donePaperCount} paper(s) remaining`;
+// }
+
+
+
+
+
+// async function showPaper(id) {
+//     const students = await getPaper(id);
+
+
+
+//     let donePaperCount = 0;
+//     let paperCount = 0;
+
+//     for (let i = 1; i <= students.length; i++) {
+//         students.forEach(student => {
+//             if (student.ID === id) {
+//                 if (student['Personal id'] && student.Certificate && student['HR Letter'] && student['HR Letter'] === true) {
+//                     paperCount++;
+//                     console.log(paperCount);
+//             }}
+//         });
+//     }
+//     // Hide spinner element
+//     // document.body.removeChild(spinner);
+
+//     // Update the module count
+//     const moduleCountElement = document.getElementById('moduleCount');
+//     moduleCountElement.textContent = `${totalDoneModules} / ${moduleCount}`;
+
+//     // Update the footer based on the next module deadline
+//     const filteredModules = students.filter(student => {
+//         const date = new Date(student[`g${i} date`]);
+//         return date >= new Date();
+//     });
+//     const nextModule = filteredModules.sort((a, b) => new Date(a[`g${i} date`]) - new Date(b[`g${i} date`]))[0];
+//     if (nextModule) {
+//         const formattedDueDate = new Intl.DateTimeFormat('en-US', { month: 'short', day: 'numeric', year: 'numeric' }).format(new Date(nextModule[`g${i} date`]));
+//         footer4.textContent = `Next Module: ${formattedDueDate}`;
+//     } else {
+//         footer4.textContent = 'No upcoming Module';
+//     }
+//     let moduleUrl = `Group.html?id=${id}`;
+//     seeMore4.href = moduleUrl;
+//     let module = await fetch(moduleUrl);
+//     let moduleData = await module.json();
+//     localStorage.setItem('moduleData', JSON.stringify(moduleData));
+//     window.open(moduleUrl);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
